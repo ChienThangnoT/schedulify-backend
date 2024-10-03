b8e6585fa28c0f2c29815fd944ee389151b2119a
thanhlong109 <thinghiemminiworld@gmail.com>
2024-10-03 17:07:30 +0700

    fix: fix teacher response model, fix get teacher by schools

--- SchedulifySystem.Service/BusinessModels/TeacherBusinessModels/TeacherViewModel.cs
+++ SchedulifySystem.Service/BusinessModels/TeacherBusinessModels/TeacherViewModel.cs
@@ -13,13 +13,17 @@ namespace SchedulifySystem.Service.BusinessModels.TeacherBusinessModels
         public string? LastName { get; set; }
         public string? Abbreviation { get; set; }
         public string? Email { get; set; }
-        public int Gender { get; set; }
+        public string? Gender { get; set; }
         public int DepartmentId { get; set; }
+        public string? DepartmentName { get; set; }
+        public string? TeacherGroupName { get; set; }
         public DateOnly DateOfBirth { get; set; }
-        public int SchoolId { get; set; }
         public int TeacherGroupId { get; set; }
         public int TeacherRole { get; set; }
         public int Status { get; set; }
         public bool IsDeleted { get; set; }
+        public string? Phone {  get; set; }
+        public List<string> TeachableSubjects { get; set; } = new List<string>();
+        
     }
 }
