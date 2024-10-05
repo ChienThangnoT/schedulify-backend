99346e0b4b7a2c96a018b114b06e087c2e281688
thanhlong109 <thinghiemminiworld@gmail.com>
2024-10-06 00:41:13 +0700

    fix: fix abbreviation dupplicate, fix add teacher list

---
+++ SchedulifySystem.Service/Utils/AbbreviationUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulifySystem.Service.Utils
+{
+    public static class AbbreviationUtils
+    {
+        public static string GenerateUniqueAbbreviation(string baseAbbreviation, IEnumerable<string> existingAbbreviations)
+        {
+            var newAbbreviation = baseAbbreviation.ToLower();
+            int counter = 1;
+
+            // Check for any conflicts with existing abbreviations
+            while (existingAbbreviations.Any(a => a.Equals(newAbbreviation, StringComparison.OrdinalIgnoreCase)))
+            {
+                var parsedAbbreviation = ParseAbbreviation(baseAbbreviation);
+                newAbbreviation = $"{parsedAbbreviation.textPart}{parsedAbbreviation.numberPart + counter}";
+                counter++;
+            }
+
+            return newAbbreviation;
+        }
+
+        private static (string textPart, int numberPart) ParseAbbreviation(string baseAbbreviation)
+        {
+            var match = System.Text.RegularExpressions.Regex.Match(baseAbbreviation, @"^(.*?)(\d+)$");
+            if (match.Success)
+            {
+                return (match.Groups[1].Value, int.Parse(match.Groups[2].Value));
+            }
+            return (baseAbbreviation, 0); // No number found
+        }
+    }
+}
