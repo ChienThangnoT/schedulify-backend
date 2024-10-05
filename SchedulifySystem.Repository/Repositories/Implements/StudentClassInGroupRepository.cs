02ee9b831816590413c533e4fead464b4e09bddb
thanhlong109 <thinghiemminiworld@gmail.com>
2024-10-05 09:42:16 +0700

    feat: add class in group repo

---
+++ SchedulifySystem.Repository/Repositories/Implements/StudentClassInGroupRepository.cs
@@ -0,0 +1,18 @@
+﻿using SchedulifySystem.Repository.DBContext;
+using SchedulifySystem.Repository.EntityModels;
+using SchedulifySystem.Repository.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulifySystem.Repository.Repositories.Implements
+{
+    public class StudentClassInGroupRepository : GenericRepository<StudentClassInGroup>, IStudentClassInGroupRepository
+    {
+        public StudentClassInGroupRepository(SchedulifyContext context) : base(context)
+        {
+        }
+    }
+}
