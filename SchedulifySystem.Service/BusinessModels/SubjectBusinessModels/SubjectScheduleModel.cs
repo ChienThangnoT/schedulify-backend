1e256fde9de36546a2453fe03bd214e3408a97ed
thanhlong109 <thinghiemminiworld@gmail.com>
2024-10-15 17:59:36 +0700

    feat: add generate schedule business model

---
+++ SchedulifySystem.Service/BusinessModels/SubjectBusinessModels/SubjectScheduleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulifySystem.Service.BusinessModels.SubjectBusinessModels
+{
+    public class SubjectScheduleModel
+    {
+        public int Id { get; set; }
+        public string? SubjectName { get; set; }
+        public string? Abbreviation { get; set; }
+        public bool IsRequired { get; set; }
+    }
+}
