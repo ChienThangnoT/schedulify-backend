03f3dfc6f3b6e82de857ddd14de1c13c2f123fac
thanhlong109 <thinghiemminiworld@gmail.com>
2024-10-15 10:04:31 +0700

    feat: update building response message

--- SchedulifySystem.Service/Utils/Constants/ConstantResponse.cs
+++ SchedulifySystem.Service/Utils/Constants/ConstantResponse.cs
@@ -21,24 +21,33 @@ namespace SchedulifySystem.Service.Utils.Constants
         public const string RESET_PASSWORD_SUCCESS = "Reset password success.";
 
         //Room
-        public const string ROOM_NOT_EXIST = "The room is not exist.";
-        public const string ROOM_NAME_EXISTED = "The room name is existed.";
+        public const string ROOM_NOT_EXIST = "The room does not exist.";
+        public const string ROOM_NAME_EXISTED = "The room name already exists.";
         public const string ROOM_NAME_DUPLICATED = "The room name is duplicated";
-        public const string ROOM_CODE_EXISTED = "The room code is existed.";
+        public const string ROOM_CODE_EXISTED = "The room code already exists.";
         public const string ROOM_CODE_DUPLICATED = "The room code is duplicated";
-        public const string ROOM_CODE_OR_NAME_EXISTED = "The room code or room name is existed.";
+        public const string ROOM_CODE_OR_NAME_EXISTED = "The room code or room name already exists.";
         public const string ADD_ROOM_SUCCESS = "Add room success.";
         public const string UPDATE_ROOM_SUCCESS = "Update room success.";
         public const string DELETE_ROOM_SUCCESS = "Delete room success.";
-        public const string GET_ROOM_SUCCESS = "Get room success.";
+        public const string GET_ROOM_SUCCESS = "Get rooms success.";
 
         //Building
-        public const string BUILDING_NOT_EXIST = "The building is not exist.";
-        public const string BUILDING_CODE_NOT_EXIST = "The building code is not exist.";
+        public const string BUILDING_NOT_EXIST = "The building does not exist.";
+        public const string BUILDING_NAME_EXISTED = "The building name already exists.";
+        public const string BUILDING_NAME_DUPLICATED = "The building name is duplicated";
+        public const string BUILDING_CODE_NOT_EXIST = "The building code does not exist.";
+        public const string BUILDING_CODE_EXISTED = "The building code already exists.";
+        public const string BUILDING_CODE_DUPLICATED = "The building code is duplicated";
+        public const string BUILDING_CODE_OR_NAME_EXISTED = "The building code or building name already exists.";
+        public const string ADD_BUILDING_SUCCESS = "Add building success.";
+        public const string UPDATE_BUILDING_SUCCESS = "Update building success.";
+        public const string DELETE_BUILDING_SUCCESS = "Delete building success.";
+        public const string GET_BUILDING_SUCCESS = "Get buildings success.";
 
 
         //Room Type
-        public const string ROOM_TYPE_NOT_EXIST = "The room type is not exist.";
-        public const string ROOM_TYPE_CODE_NOT_EXIST = "The room type code is not exist.";
+        public const string ROOM_TYPE_NOT_EXIST = "The room type does not exist.";
+        public const string ROOM_TYPE_CODE_NOT_EXIST = "The room type code does not exist.";
     }
 }
