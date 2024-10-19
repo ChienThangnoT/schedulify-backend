20687bc18e84eba276d4f41cc483bc6d0d5c6512
ChienThangNoT <chienthang014@gmail.com>
2024-10-19 23:45:08 +0700

    fix: update subjectingroup table to db

---
+++ SchedulifySystem.Repository/Migrations/20241019164401_UpdateSubjectInGroupTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchedulifySystem.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSubjectInGroupTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SlotPerWeek",
+                table: "SubjectInGroups",
+                newName: "MoringSlotPerWeek");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AfternoonSlotPerWeek",
+                table: "SubjectInGroups",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDoublePeriod",
+                table: "SubjectInGroups",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AfternoonSlotPerWeek",
+                table: "SubjectInGroups");
+
+            migrationBuilder.DropColumn(
+                name: "IsDoublePeriod",
+                table: "SubjectInGroups");
+
+            migrationBuilder.RenameColumn(
+                name: "MoringSlotPerWeek",
+                table: "SubjectInGroups",
+                newName: "SlotPerWeek");
+        }
+    }
+}
