9598fe7a165051383eee61b0a8a3500aff3df764
ChienThangNoT <chienthang014@gmail.com>
2024-10-23 02:00:22 +0700

    fix: update controller

--- SchedulifySystem.Service/BusinessModels/SubjectBusinessModels/SubjectAddModel.cs
+++ SchedulifySystem.Service/BusinessModels/SubjectBusinessModels/SubjectAddModel.cs
@@ -1,6 +1,7 @@
 ﻿using SchedulifySystem.Service.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -27,10 +28,9 @@ namespace SchedulifySystem.Service.BusinessModels.SubjectBusinessModels
         public required string Abbreviation { get; set; }
         public bool IsRequired { get; set; }
         public required string Description { get; set; }
-        [JsonIgnore]
-        public int? TotalSlotInYear { get; set; }
-        [JsonIgnore]
-        public int? SlotSpecialized { get; set; } = 35;
+        public int TotalSlotInYear { get; set; }
+        [DefaultValue(35)]
+        public int SlotSpecialized { get; set; }
         public ESubjectGroupType SubjectGroupType { get; set; }
 
     }
