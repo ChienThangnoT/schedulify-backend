7b05c742531d484c354e0688e859ba6df1cd1dbc
thanhlong109 <thinghiemminiworld@gmail.com>
2024-10-22 17:03:20 +0700

    feat: update para

--- SchedulifySystem.Service/BusinessModels/ClassPeriodBusinessModels/ClassPeriodScheduleModel.cs
+++ SchedulifySystem.Service/BusinessModels/ClassPeriodBusinessModels/ClassPeriodScheduleModel.cs
@@ -14,37 +14,37 @@ namespace SchedulifySystem.Service.BusinessModels.ClassPeriodBusinessModels
 {
     public record ClassPeriodScheduleModel 
     {
-        [JsonIgnore]
+
         public int Id { get; set; }
-        [JsonIgnore]
+
         public int? ClassScheduleId { get; set; }
-        [JsonIgnore]
+
         public int? TimeSlotId { get; set; }
-        [JsonIgnore]
+
         public int? RoomId { get; set; }
-        [JsonIgnore]
+
         public int? Status { get; set; }
-        [JsonIgnore]
+
         public int? TeacherId { get; set; }
         public int? SubjectId { get; set; }
-        [JsonIgnore]
+
         public int? DateOfWeek { get; set; }
         public int? ClassId { get; set; }
-        [JsonIgnore]
+
         public string? ClassName { get; set; }
-        [JsonIgnore]
+        
         public string? SubjectAbbreviation { get; set; }
-        [JsonIgnore]
+        
         public string? TeacherAbbreviation { get; set; }
-        [JsonIgnore]
+        
         public string? RoomCode { get; set; }
-        [JsonIgnore]
+        
         public int? TeacherAssignmentId { get; set; }
         
         public int StartAt { get; set; }
-        [JsonIgnore]
+        
         public EPriority Priority { get; set; } = EPriority.None;
-        [JsonIgnore]
+        
         public List<ConstraintErrorModel> ConstraintErrors { get; set; } = [];
 
         public ClassPeriodScheduleModel()
