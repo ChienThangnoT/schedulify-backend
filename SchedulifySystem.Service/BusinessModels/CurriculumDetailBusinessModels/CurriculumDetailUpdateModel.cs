8de3b6ea54b6a21b08d596ba3fd39ff8cf8df097
thanhlong109 <thinghiemminiworld@gmail.com>
2024-11-17 18:46:47 +0700

    fix: fix request model update curriculum detail

--- SchedulifySystem.Service/BusinessModels/CurriculumDetailBusinessModels/CurriculumDetailUpdateModel.cs
+++ SchedulifySystem.Service/BusinessModels/CurriculumDetailBusinessModels/CurriculumDetailUpdateModel.cs
@@ -12,11 +12,7 @@ namespace SchedulifySystem.Service.BusinessModels.CurriculumDetailBusinessModels
         public int MainSlotPerWeek { get; set; }
         public int SubSlotPerWeek { get; set; }
         public int SlotPerTerm { get; set; }
-        public int? TermId { get; set; }
-        public int? CurriculumId { get; set; }
-        public bool IsSpecialized { get; set; }
         public bool IsDoublePeriod { get; set; }
-        public int SubjectInGroupType { get; set; }
         public int MainMinimumCouple { get; set; }
         public int SubMinimumCouple { get; set; }
 
