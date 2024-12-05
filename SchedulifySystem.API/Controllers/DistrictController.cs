b75ee1e0014e2d92f865e279165d9eb2e78b9aac
thanhlong109 <thinghiemminiworld@gmail.com>
2024-12-05 14:08:00 +0700

    feat: add manage district

--- SchedulifySystem.API/Controllers/DistrictController.cs
+++ SchedulifySystem.API/Controllers/DistrictController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchedulifySystem.Service.BusinessModels.DistrictBusinessModels;
 using SchedulifySystem.Service.Services.Interfaces;
 
 namespace SchedulifySystem.API.Controllers
@@ -15,10 +16,28 @@ namespace SchedulifySystem.API.Controllers
             _districtService = districtService;
         }
 
-        [HttpGet("provinces/{id}")]
-        public Task<IActionResult> GetDistrictByProvinceId(int id, int pageIndex = 1, int pageSize = 20)
+        [HttpGet("provinces/{provinceId}")]
+        public Task<IActionResult> GetDistrictByProvinceId(int provinceId, int pageIndex = 1, int pageSize = 20)
         {
-            return ValidateAndExecute(() => _districtService.GetDistrictByProvinceId(id, pageIndex, pageSize));
+            return ValidateAndExecute(() => _districtService.GetDistrictByProvinceId(provinceId, pageIndex, pageSize));
+        }
+
+        [HttpPost("provinces/{provinceId}")]
+        public Task<IActionResult> AddDistricts(int provinceId, List<DistrictAddModel> models)
+        {
+            return ValidateAndExecute(() => _districtService.AddDistricts(provinceId,models));
+        }
+
+        [HttpPut("/{districtCode}/provinces/{provinceId}")]
+        public Task<IActionResult> UpdateDistricts(int provinceId, int districtCode,DistrictUpdateModel model)
+        {
+            return ValidateAndExecute(() => _districtService.UpdateDistrict(provinceId, districtCode,model));
+        }
+
+        [HttpDelete("/{districtCode}/provinces/{provinceId}")]
+        public Task<IActionResult> DeleteDistricts(int provinceId, int districtCode)
+        {
+            return ValidateAndExecute(() => _districtService.DeleteDistrict(provinceId, districtCode));
         }
     }
 }
--- SchedulifySystem.Service/Services/Implements/DistrictService.cs
+++ SchedulifySystem.Service/Services/Implements/DistrictService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using SchedulifySystem.Repository.Commons;
 using SchedulifySystem.Repository.EntityModels;
 using SchedulifySystem.Service.BusinessModels.DistrictBusinessModels;
@@ -27,6 +28,55 @@ namespace SchedulifySystem.Service.Services.Implements
             _mapper = mapper;
         }
 
+        public async Task<BaseResponseModel> AddDistricts(int provinceId, List<DistrictAddModel> models)
+        {
+            var province = await _unitOfWork.ProvinceRepo.GetByIdAsync(provinceId, filter: f => !f.IsDeleted, 
+                include: query => query.Include(p => p.Districts.Where(d => !d.IsDeleted).OrderByDescending(d => d.DistrictCode))) ??
+                throw new NotExistsException(ConstantResponse.PROVINCE_NOT_EXIST);
+
+            var founded = province.Districts.Where(d => models.Select(s => s.Name.Trim().ToLower()).Contains(d.Name.Trim().ToLower()));
+            if (founded.Any())
+            {
+                return new BaseResponseModel()
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Message = $"Quận/huyện {string.Join(", ", founded.Select(s => s.Name))} đã tồn tại trong hệ thống."
+                };
+            }
+            var lastCode = province.Districts.FirstOrDefault()?.DistrictCode ?? 0;
+            var districts = _mapper.Map<List<District>>(models);
+            foreach (var district in districts)
+            {
+                district.ProvinceId = provinceId;
+                district.DistrictCode = ++lastCode;
+            }
+            await _unitOfWork.DistrictRepo.AddRangeAsync(districts);
+            await _unitOfWork.SaveChangesAsync();
+            return new BaseResponseModel()
+            {
+                Status = StatusCodes.Status200OK,
+                Message = ConstantResponse.ADD_DISTRICT_SUCCESS
+            };
+        }
+
+        public async Task<BaseResponseModel> DeleteDistrict(int provinceId, int districtCode)
+        {
+            var province = await _unitOfWork.ProvinceRepo.GetByIdAsync(provinceId, filter: f => !f.IsDeleted) ??
+                throw new NotExistsException(ConstantResponse.PROVINCE_NOT_EXIST);
+
+            var district = (await _unitOfWork.DistrictRepo.GetV2Async(filter: f => f.Province.Id == provinceId && f.DistrictCode == districtCode && !f.IsDeleted)).FirstOrDefault() ??
+                throw new NotExistsException(ConstantResponse.DISTRICT_NOT_EXIST);
+
+            district.IsDeleted = true;
+            _unitOfWork.DistrictRepo.Update(district);
+            await _unitOfWork.SaveChangesAsync();
+            return new BaseResponseModel()
+            {
+                Status = StatusCodes.Status200OK,
+                Message = ConstantResponse.REMOVE_DISTRICT_SUCCESS
+            };
+        }
+
         public async Task<BaseResponseModel> GetDistrictByProvinceId(int? provinceId, int pageIndex, int pageSize)
         {
             var districts = await _unitOfWork.DistrictRepo.GetPaginationAsync(
@@ -46,6 +96,41 @@ namespace SchedulifySystem.Service.Services.Implements
                 Status = StatusCodes.Status200OK,
                 Message = ConstantResponse.GET_DISTRICT_SUCCESS,
                 Result = result
+            };
+        }
+
+        public async Task<BaseResponseModel> UpdateDistrict(int provinceId, int districtCode, DistrictUpdateModel model)
+        {
+            var province = await _unitOfWork.ProvinceRepo.GetByIdAsync(provinceId, filter: f => !f.IsDeleted,
+                include: query => query.Include(p => p.Districts.Where(d => !d.IsDeleted).OrderByDescending(d => d.DistrictCode))) ??
+                throw new NotExistsException(ConstantResponse.PROVINCE_NOT_EXIST);
+
+            var district = province.Districts.FirstOrDefault(d => d.DistrictCode == districtCode) ??
+               throw new NotExistsException(ConstantResponse.DISTRICT_NOT_EXIST);
+
+            var oldName = district.Name.Trim().ToLower();
+            var newName = model.Name.Trim().ToLower();
+
+            if(oldName != newName)
+            {
+                var founded = province.Districts.Where(d => newName == d.Name.Trim().ToLower());
+                if (founded.Any())
+                {
+                    return new BaseResponseModel()
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Message = $"Quận/huyện {model.Name} đã tồn tại trong hệ thống."
+                    };
+                }
+                district.Name = model.Name.Trim();
+            }
+            _unitOfWork.DistrictRepo.Update(district);
+            await _unitOfWork.SaveChangesAsync();
+
+            return new BaseResponseModel()
+            {
+                Status = StatusCodes.Status200OK,
+                Message = ConstantResponse.UPDATE_DISTRICT_SUCCESS
             };
         }
     }
