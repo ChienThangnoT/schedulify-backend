e00d51bf4434c60d5fed687b7e3eb0ceabaa812c
ChienThangNoT <chienthang014@gmail.com>
2024-12-15 02:29:45 +0700

    fix: update timetable

---
+++ SchedulifySystem.Repository/Migrations/20241214190331_UpdateClassPeriod.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchedulifySystem.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClassPeriod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeacherAssignmentId",
+                table: "ClassPeriod");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherAssignmentId",
+                table: "ClassPeriod",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
